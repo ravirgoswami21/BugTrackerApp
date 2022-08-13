3ef1f9665fc790fcf8dff932a4889dbf6041ad99
PathShalaApp <59417019+pathshalaappofficial@users.noreply.github.com>
2022-08-14 02:09:22 +0530

    RatePay-1 : Added Sample RestController

--- bugtracker/src/main/java/com/ratepay/app/bugtracker/BugTrackerAppApplication.java
+++ bugtracker/src/main/java/com/ratepay/app/bugtracker/BugTrackerAppApplication.java
@@ -1,7 +1,12 @@
 package com.ratepay.app.bugtracker;
 
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BugTrackerAppApplication {
@@ -10,4 +15,19 @@ public class BugTrackerAppApplication {
 		SpringApplication.run(BugTrackerAppApplication.class, args);
 	}
 
+//	@Bean
+//	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+//		return args -> {
+//
+//			System.out.println("Let's inspect the beans provided by Spring Boot:");
+//
+//			String[] beanNames = ctx.getBeanDefinitionNames();
+//			Arrays.sort(beanNames);
+//			for (String beanName : beanNames) {
+//				System.out.println(beanName);
+//			}
+//
+//		};
+//	}
+	
 }
