d32ff66121bd5567c04a5069120b098380d830cb
PathShalaApp <59417019+pathshalaappofficial@users.noreply.github.com>
2022-08-14 01:44:03 +0530

    RatePay-1 : SpringBoot Project initiallization

---
+++ bugtracker/src/main/java/com/ratepay/app/bugtracker/BugTrackerAppApplication.java
@@ -0,0 +1,13 @@
+package com.ratepay.app.bugtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BugTrackerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BugTrackerAppApplication.class, args);
+	}
+
+}
